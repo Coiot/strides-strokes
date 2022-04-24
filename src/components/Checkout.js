151ec4cb17f43c4f46582b4ead20cc145e6967e2
Isaac Volpe <isaac.volpe@gmail.com>
2022-04-24 16:43:26 +0000

    *

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -41,7 +41,7 @@ const Checkout = () => {
     setInputQuantityValue(parseInt(event.target.value));
   };
 
-  const [inputGymValue, setInputGymValue] = useState("");
+  const [inputGymValue, setInputGymValue] = useState("price_1IIo5IFE8xsP1GiTIeBjrCPc");
 
   const onChangeGymHandler = (event) => {
     setInputGymValue(parseInt(event.target.value));
